1f546a665cc668655750e43b8807d33aa0780802
Mark Eschbach <meschbach@gmail.com>
2020-12-23 14:15:54 -0800

    wui: Clear chat on room change

--- multi-user-conference/src/playing/chat-view.js
+++ multi-user-conference/src/playing/chat-view.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {RoomEvents} from "muc-client/client";
+import {States} from "../game-state";
 
 export function ViewRoomChat({controller}){
 	const [messages,setMessages] = useState([]);
@@ -8,13 +9,18 @@ export function ViewRoomChat({controller}){
 		const onRoomMessage = (message) => {
 			setMessages([].concat(messages, [message]));
 		}
+		const onNewRoom = () => {
+			setMessages([]);
+		}
 		controller.client.on(RoomEvents.ChatBroadcast, onRoomMessage);
+		controller.on(States.NewRoom, onNewRoom);
 		return () => {
 			controller.client.off(RoomEvents.ChatBroadcast, onRoomMessage);
+			controller.off(States.NewRoom, onNewRoom);
 		}
-	}, [controller, messages]);
+	}, [controller]);
 
 	return (<ul>
-		{messages.map((m, index) => <li key={index}>{JSON.stringify(m)}: {m.what}</li>)}
+		{messages.map((m, index) => <li key={index}>{m.from}: {m.what}</li>)}
 	</ul>);
 }
